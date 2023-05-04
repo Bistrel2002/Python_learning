3fc02362b1c05bc67698031998bcfe70fe71c4cd
Tsangue <bistrel.tsangue@algosup.com>
2023-05-05 00:29:31 +0200

    Update Début.py

--- Début.py
+++ Début.py
@@ -4,7 +4,6 @@ class Human:
     """
     Human's class
     """
-
     Human_created = 0
     def __init__(self):
         print("Creation of a Humain...")
@@ -15,16 +14,13 @@ class Human:
         self.height = input("Height")
         self.height = float(self.height)
         Human.Human_created += 1 
-    def say(self, message):
-        self.message = "Hi to everyone"
-        print("{} said {}",format(self.lname, self.message))
-    
+
+        
 print("Beginning of the project...")
 
 H1 = Human()
-print("First Name: {}", format(H1.fname))
+print("First Name: {}", format(H1.fname))+
 print("Last Name: {}", format(H1.lname))
-H1.say()
 print("Age: {}", format(H1.age), "years old")
 print("Heigth: {}", format(H1.height),"cm")
 
